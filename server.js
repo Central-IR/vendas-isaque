3a9b2b91216fac9b85903df59a37e4a4b6b35ed6
I.R. COMERCIO E MATERIAIS ELETRICOS LTDA <miguel.alves1@faculdadegran.edu.br>
2026-01-10 23:24:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,25 @@ const supabase = createClient(
 );
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Session-Token'],
+  credentials: false
+}));
+
+// Headers adicionais de CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Session-Token');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.static('public'));
 
